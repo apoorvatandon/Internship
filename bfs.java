5cfa4246901a422066573c184a8e2b11cc995e86
apoorvatandon <55032664+apoorvatandon@users.noreply.github.com>
2022-08-18 11:54:18 +0530

    Create bfs.java

---
+++ bfs.java
@@ -0,0 +1,16 @@
+public class T_BFS {
+
+	public void levelOrderQueue(Node root) {
+		Queue<Node> q = new LinkedList<Node>();
+		if (root == null)
+			return;
+		q.add(root);
+		while (!q.isEmpty()) {
+			Node n = (Node) q.remove();
+			System.out.print(" " + n.data);
+			if (n.left != null)
+				q.add(n.left);
+			if (n.right != null)
+				q.add(n.right);
+		}
+	}
